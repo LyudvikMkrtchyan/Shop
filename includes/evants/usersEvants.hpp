7b669ab3723dfc66e3ee9b7378cc2565f777bdfa
TerAper <shokhak.ter.sargsyan@gmail.com>
2024-11-21 19:24:09 +0400

    Aper

--- includes/evants/usersEvants.hpp
+++ includes/evants/usersEvants.hpp
@@ -6,5 +6,4 @@ using FunctionPtr = void(*)(const request&, response&, DataBase&);
 
 void login(const request &req, response &res, DataBase &db);
 
-void foo();
 void usersEvantsInitalaizer(EvantSwitch& evantSwitch);
